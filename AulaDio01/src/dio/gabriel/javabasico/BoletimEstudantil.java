b2499fa327eb65f9618e1af9da4c10b957e8dc99
mmoraesgabriell <mussatocarlos29@gmail.com>
2024-06-17 15:16:46 -0300

    criando formula para ver a mediaFinal

--- AulaDio01/src/dio/gabriel/javabasico/BoletimEstudantil.java
+++ AulaDio01/src/dio/gabriel/javabasico/BoletimEstudantil.java
@@ -3,7 +3,7 @@ package dio.gabriel.javabasico;
 
 public class BoletimEstudantil {
     public static void main(String[] args){
-        int mediaFinal = 6;
+        int mediaFinal = 7;
         if(mediaFinal < 6 )
             System.out.println("REPROVADO");
         else if(mediaFinal == 6)
