3fd027d80bc3d74650a562d9a79a2a66ae46a419
mmoraesgabriell <mussatocarlos29@gmail.com>
2024-06-17 15:35:40 -0300

    tipos variaveis em java e aprendendo sobre final e charge

---
+++ AulaDio01/src/dio/gabriel/javabasico/TiposVariaveis.java
@@ -0,0 +1,19 @@
+package dio.gabriel.javabasico;
+
+public class TiposVariaveis {
+    public static void main(String[] args){
+        byte idade = 18;
+        short ano = 2024;
+        int cep = 8420480;
+        long cpf = 48962364832L;
+        float pi = 3.14f;
+        double salario = 8500.20;
+
+        //para não deixarmos uma variavel mudar de valor, precisamos colocar final antes do nome dela
+        final float PI = 3.14f;
+
+
+
+
+    }
+}
